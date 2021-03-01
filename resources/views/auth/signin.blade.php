9f9cc763bbbd20964544eb185f45eb599306a451
muhammadsyazili <09021181520135@students.ilkom.unsri.ac.id>
2021-03-01 13:07:03 +0700

    edit signin and signup view

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -1,62 +1,73 @@
-@extends('../layouts/app', ['layout' => 'signin'])
+@extends('../layouts/app-v1')
 
 @section('title', 'Sign In')
 
 @section('content')
 <div class="login-box">
     <div class="login-logo">
-        <h1>Muslim <strong>Up</strong></h1>
+        <h1><b>{{__('Muslim')}}</b> {{__('Up')}}</h1>
     </div>
     <!-- /.login-logo -->
     <div class="card">
         <div class="card-body login-card-body">
-        <p class="login-box-msg">Sign In</p>
+        <p class="login-box-msg">{{__('Sign In')}}</p>
 
-        <form action="index3.html" method="post">
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            @method('POST')
+
             <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email" />
-            <div class="input-group-append">
-                <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                    <span class="fas fa-at"></span>
+                    </div>
                 </div>
+                <input type="text" class="form-control @error('username') is-invalid @enderror" placeholder="Username" name="username" value="{{ old('username') }}"/>
             </div>
-            </div>
+
+            @error('username')
+                <div class="alert alert-danger mt-2">
+                    {{ $message }}
+                </div>    
+            @enderror
+
             <div class="input-group mb-3">
-            <input
-                type="password"
-                class="form-control"
-                placeholder="Password"
-            />
-            <div class="input-group-append">
-                <div class="input-group-text">
-                <span class="fas fa-lock"></span>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                    </div>
                 </div>
+                <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password"/>
             </div>
-            </div>
+
+            @error('password')
+                <div class="alert alert-danger mt-2">
+                    {{ $message }}
+                </div>    
+            @enderror
+
             <div class="row">
-            <div class="col-12">
-                <div class="icheck-primary">
-                <input type="checkbox" id="remember" />
-                <label for="remember"> Remember Me </label>
+                <div class="col-12">
+                    <div class="icheck-primary">
+                        <input type="checkbox" id="remember" name="remember"/>
+                        <label for="remember">{{__('Remember Me')}}</label>
+                    </div>
                 </div>
-            </div>
-            <!-- /.col -->
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary btn-block">
-                Sign In
-                </button>
-            </div>
-            <!-- /.col -->
+                <!-- /.col -->
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary btn-block">
+                        {{__('Sign In')}}
+                    </button>
+                </div>
+                <!-- /.col -->
             </div>
         </form>
 
         <p class="mb-1">
-            <a href="forgot-password.html">I forgot my password</a>
+            <a href="{{ route('password.request') }}">{{__('I forgot my password')}}</a>
         </p>
         <p class="mb-0">
-            <a href="register.html" class="text-center"
-            >Register a new membership</a
-            >
+            <a href="{{ route('register') }}" class="text-center">{{__('Sign up a new membership')}}</a>
         </p>
         </div>
         <!-- /.login-card-body -->
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,80 +1,145 @@
-@extends('../layouts/app', ['layout' => 'signup'])
+@extends('../layouts/app-v1')
 
 @section('title', 'Sign Up')
 
 @section('content')  
 <div class="register-box">
   <div class="register-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <h1><b>{{__('Muslim')}}</b> {{__('Up')}}</h1>
   </div>
 
   <div class="card">
     <div class="card-body register-card-body">
-      <p class="login-box-msg">Register a new membership</p>
+      <p class="login-box-msg">{{__('Sign up a new membership')}}</p>
 
-      <form action="../../index.html" method="post">
+      <form action="{{ route('register') }}" method="post">
+        @csrf
+        @method('POST')
+
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Full name">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-user"></span>
+              <span class="fas fa-at"></span>
             </div>
           </div>
+          <input type="text" class="form-control @error('username') is-invalid @enderror" placeholder="Username" value="{{ old('username') }}" name="username">
         </div>
+
+        @error('username')
+            <div class="alert alert-danger mt-2">
+                {{ $message }}
+            </div>    
+        @enderror
+
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
             </div>
           </div>
+          <input type="text" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}" name="email">
         </div>
+
+        @error('email')
+            <div class="alert alert-danger mt-2">
+                {{ $message }}
+            </div>    
+        @enderror
+
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+          <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Full Name" value="{{ old('name') }}" name="name">
+        </div>
+
+        @error('name')
+            <div class="alert alert-danger mt-2">
+                {{ $message }}
+            </div>    
+        @enderror
+
+        <div class="input-group mb-3">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-birthday-cake"></span>
+            </div>
+          </div>
+          <input type="date" class="form-control @error('birthday') is-invalid @enderror" placeholder="Birthday" value="{{ old('birthday') }}" name="birthday">
+        </div>
+
+        @error('birthday')
+            <div class="alert alert-danger mt-2">
+                {{ $message }}
+            </div>    
+        @enderror
+
+        <div class="form-group clearfix">
+          <div class="icheck-primary d-inline">
+            <input type="radio" id="gender-male" name="gender" value="male" checked>
+            <label for="gender-male">
+              {{__('Male')}}
+            </label>
+          </div>
+          <div class="icheck-primary d-inline">
+            <input type="radio" id="gender-female" name="gender" value="female">
+            <label for="gender-female">
+              {{__('Female')}}
+            </label>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
+          <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password">
         </div>
+
+        @error('password')
+            <div class="alert alert-danger mt-2">
+                {{ $message }}
+            </div>    
+        @enderror
+
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
+          <input type="password" class="form-control" placeholder="Retype password" name="password_confirmation">
         </div>
+
         <div class="row">
-          <div class="col-8">
+          <div class="col-12">
             <div class="icheck-primary">
-              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+              <input type="checkbox" id="agreeTerms" name="terms" value="yes">
               <label for="agreeTerms">
-                I agree to the <a href="#">terms</a>
+                {{__('I agree to the')}} <a href="/term">{{__('terms')}}</a>
               </label>
             </div>
+
+            @error('terms')
+              <div class="alert alert-danger mt-2">
+                  {{ $message }}
+              </div>    
+            @enderror
           </div>
           <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
+
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block">{{__('Sign Up')}}</button>
           </div>
           <!-- /.col -->
         </div>
       </form>
 
-      <div class="social-auth-links text-center">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i>
-          Sign up using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i>
-          Sign up using Google+
-        </a>
-      </div>
-
-      <a href="login.html" class="text-center">I already have a membership</a>
+      <a href="{{ route('login') }}" class="text-center">{{__('I already have a membership')}}</a>
     </div>
     <!-- /.form-box -->
   </div><!-- /.card -->
